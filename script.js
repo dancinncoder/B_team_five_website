b9d49771f1194775b863fa182166d6f3ff81610e
volant97 <volant97@naver.com>
2023-10-06 16:03:03 +0900

    team_intro push

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ function memberToggle5(){
   $('#informationBox_지훈').toggle(1000);
 }
 
+
+
+// team intro part
+const saEnter = document.querySelector(".saEnter")
+const teamPicture = document.querySelector(".team_picture")
+const HIDDEN_CLASSNAME = "hidden";
+
+function saMouseenter() {
+  if (saEnter.classList = "hidden") {
+    saEnter.classList.remove(HIDDEN_CLASSNAME);
+    console.log("mouseenter");
+  } else {
+    saEnter.classList.add(HIDDEN_CLASSNAME);
+    console.log("MouseLeaver");
+  }
+}
+
+const saEventHandler = {
+  'Mouseenter': () => {
+    saEnter.classList.remove(HIDDEN_CLASSNAME);
+    console.log("mouseenter");x
+  },
+  mouseleave: function saMouseLeave() {
+    saEnter.classList.add(HIDDEN_CLASSNAME);
+    console.log("MouseLeaver");
+  }
+};
+
+function openSA() {
+  console.log("click");
+}
+
+saEnter.classList.add(HIDDEN_CLASSNAME);
+teamPicture.addEventListener("mouseenter", saEventHandler.Mouseenter);
+teamPicture.addEventListener("mouseleave", saEventHandler.mouseleave);
+saEnter.addEventListener("click", openSA);
