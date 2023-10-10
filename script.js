7169b4b74b6d45f6734a30115a64e7d8f6069730
volant97 <volant97@naver.com>
2023-10-10 21:12:54 +0900

    10.10 마무리회의 후 수정

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 /* <-----------------------------member detail toggle(지훈,하민)----------------------------> */
-
+const infor_하민 = document.getElementById("informationBox_하민");
+const infor_창근 = document.getElementById("informationBox_창근");
+const infor_준호 = document.getElementById("informationBox_준호");
+const infor_지훈 = document.getElementById("informationBox_지훈");
 
 function memberToggle1(){
-  $('#informationBox_하민').toggle(1000);
-  if (document.getElementById("informationBox_하민").style.display = "block") {
-    document.getElementById("informationBox_창근").style.display = "none";
-    document.getElementById("informationBox_준호").style.display = "none";
-    document.getElementById("informationBox_지훈").style.display = "none";
+  $("#informationBox_하민").toggle(1000);
+  if (infor_하민.style.display = "block") {
+    infor_창근.style.display = "none";
+    infor_준호.style.display = "none";
+    infor_지훈.style.display = "none";
   }
 }
 function memberToggle2(){
   $('#informationBox_창근').toggle(1000);
-  if (document.getElementById("informationBox_창근").style.display = "block") {
-    document.getElementById("informationBox_하민").style.display = "none";
-    document.getElementById("informationBox_준호").style.display = "none";
-    document.getElementById("informationBox_지훈").style.display = "none";
+  if (infor_창근.style.display = "block") {
+    infor_하민.style.display = "none";
+    infor_준호.style.display = "none";
+    infor_지훈.style.display = "none";
   }
 }
 function memberToggle3(){
   $('#informationBox_준호').toggle(1000);
-  if (document.getElementById("informationBox_준호").style.display = "block") {
-    document.getElementById("informationBox_창근").style.display = "none";
-    document.getElementById("informationBox_하민").style.display = "none";
-    document.getElementById("informationBox_지훈").style.display = "none";
+  if (infor_준호.style.display = "block") {
+    infor_창근.style.display = "none";
+    infor_하민.style.display = "none";
+    infor_지훈.style.display = "none";
   }
 }
-function memberToggle5(){
+function memberToggle4(){
   $('#informationBox_지훈').toggle(1000);
-  if (document.getElementById("informationBox_지훈").style.display = "block") {
-    document.getElementById("informationBox_창근").style.display = "none";
-    document.getElementById("informationBox_준호").style.display = "none";
-    document.getElementById("informationBox_하민").style.display = "none";
+  if (infor_지훈.style.display = "block") {
+    infor_창근.style.display = "none";
+    infor_준호.style.display = "none";
+    infor_하민.style.display = "none";
   }
 }
 
@@ -45,10 +48,8 @@ const HIDDEN_CLASSNAME = "hidden";
 function saMouseenter() {
   if (saEnter.classList = "hidden") {
     saEnter.classList.remove(HIDDEN_CLASSNAME);
-    console.log("mouseenter");
   } else {
     saEnter.classList.add(HIDDEN_CLASSNAME);
-    console.log("MouseLeaver");
   }
 }
 
